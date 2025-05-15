24c3199aca2dafc0d3b39a66f0b53058950f4740
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2025-05-15 14:50:50 +0900

    Add initial project files including .gitignore, README, solution, and project files

---
+++ src/dupFinderApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dupFinderApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
